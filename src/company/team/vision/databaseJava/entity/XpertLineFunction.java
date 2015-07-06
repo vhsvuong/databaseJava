2d99f61656a5f6c25784dc93f5027363f853c67e
vhsvuong <son_vuong_2008@yahoo.com>
2015-07-06 16:26:26 +0700

    Fix: do not delete data with hibernate.hbm2ddl.auto in JPA

--- src/company/team/vision/databaseJava/entity/XpertLineFunction.java
+++ src/company/team/vision/databaseJava/entity/XpertLineFunction.java
@@ -8,62 +8,60 @@ import javax.persistence.NamedNativeQuery;
 import javax.persistence.SqlResultSetMapping;
 
 @Entity
-@NamedNativeQuery(name = "allXpertLineFunction", query = "SELECT HRMFUN.FUNKEY as id, HRMFUN.FUNDESC as [description], HRMFUND.HRi1A as translationLanguage, HRMFUND.FUNDESC as translation, HRMFUN.FUNCODE as functionCode "
-		+ "FROM HRMFUN LEFT JOIN HRMFUND ON HRMFUN.FUNKEY = HRMFUND.FUNKEY AND LOWER(HRMFUND.HRi1A) = LOWER(?)")
+@SqlResultSetMapping(name="xpertLineFunction", entities=@EntityResult(entityClass=XpertLineFunction.class))
+@NamedNativeQuery(name="allXpertLineFunction", 
+	query="SELECT HRMFUN.FUNKEY as id, HRMFUN.FUNDESC as [description], HRMFUND.HRi1A as translationLanguage, HRMFUND.FUNDESC as translation, HRMFUN.FUNCODE as functionCode "
+				+ "FROM HRMFUN LEFT JOIN HRMFUND ON HRMFUN.FUNKEY = HRMFUND.FUNKEY AND LOWER(HRMFUND.HRi1A) = LOWER(?)", 
+	resultSetMapping="xpertLineFunction")
 public class XpertLineFunction {
 	@Id
 	private Long id;
-
+	
 	@Column
 	private String description;
-
+	
 	@Column
 	private String translationLanguage;
-
+	
 	@Column
 	private String translation;
-
+	
 	@Column
 	private String functionCode;
-
+	
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
+	
 	public String getDescription() {
 		return description;
 	}
-
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
+	
 	public String getTranslationLanguage() {
 		return translationLanguage;
 	}
-
 	public void setTranslationLanguage(String translationLanguage) {
 		this.translationLanguage = translationLanguage;
 	}
-
+	
 	public String getTranslation() {
 		return translation;
 	}
-
 	public void setTranslation(String translation) {
 		this.translation = translation;
 	}
-
+	
 	public String getFunctionCode() {
 		return functionCode;
 	}
-
 	public void setFunctionCode(String functionCode) {
 		this.functionCode = functionCode;
 	}
-
+	
 }
