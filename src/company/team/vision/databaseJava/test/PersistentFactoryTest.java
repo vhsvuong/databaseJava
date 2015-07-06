a93a750aead65f1c0fba39cfdcc5d35f2e050ac7
vhsvuong <son_vuong_2008@yahoo.com>
2015-07-06 15:53:58 +0700

    Fix: do not delete data with hibernate.hbm2ddl.auto in JPA

--- src/company/team/vision/databaseJava/test/PersistentFactoryTest.java
+++ src/company/team/vision/databaseJava/test/PersistentFactoryTest.java
@@ -39,13 +39,9 @@ public class PersistentFactoryTest {
 	
 	@Test
 	public void shouldReturnDataUsingXrft_Integration_Test_Database() {
-		
-			AdminSettingDao dao = new AdminSettingDaoJpaImpl();
-			List<AdminSetting> result = dao.getAllAdminSetting(PersistentFactory.getEntityManager(PersistentType.PERSISTENT_INTEGRATION_TEST));
-			System.out.println("---------size: "+result.size());
-			Assert.assertEquals(1, result.size());
-		
-		
+		AdminSettingDao dao = new AdminSettingDaoJpaImpl();
+		List<AdminSetting> result = dao.getAllAdminSetting(PersistentFactory.getEntityManager(PersistentType.PERSISTENT_INTEGRATION_TEST));
+		Assert.assertEquals(1, result.size());
 	}
 	
 }
